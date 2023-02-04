c88b5bec2718143e20e4dc9400c72bae5e26d937
KevinStirling <youngks93@gmail.com>
2023-02-04 15:33:00 -0500

    beta 17 changes, character movement animations

--- Player.gd
+++ Player.gd
@@ -6,12 +6,23 @@ extends CharacterBody2D
 @onready var helm = get_node("/root/Space/Ship/Helm")
 @onready var ext_cam = get_node("/root/Space/Ship/ExternalCamera")
 
-const SPEED = 200.0
-const DEF_CAM_ZOOM = Vector2(2,2)
+@export var WALK_SPEED = 200.0
+@export var SPRINT_SPEED = 350.0
+@export var DEF_CAM_ZOOM = Vector2(2,2)
 
 var on_floor = true
 var in_helm_trigger = false
 var in_helm = false
+var sprinting = false
+var move_speed : float = WALK_SPEED :
+	set(value):
+		if value == SPRINT_SPEED:
+			sprinting = true
+		else: 
+			sprinting = false
+		move_speed = value
+	get :
+		return move_speed
 
 func _ready():
 	ship_interior.body_entered.connect(_player_entered_ship)
@@ -41,18 +52,25 @@ func _player_exited_ship(body):
 	ship_interior.monitoring = true
 	
 func _unhandled_input(event):
-		if Input.is_action_just_pressed("interact"):
-			if in_helm_trigger:
-				if in_helm :
-					print("no longer helming ship")
-					in_helm = false
-					ship.player_in_helm = false
-					$Camera.current = true
-				elif !in_helm:
-					print("now helming ship")
-					in_helm = true
-					ship.player_in_helm = true
-					ext_cam.current = true
+	if Input.is_action_pressed("sprint"):
+		move_speed = SPRINT_SPEED
+	if Input.is_action_just_released("sprint"):
+		move_speed = WALK_SPEED
+	if Input.is_action_just_pressed("interact"):
+		if in_helm_trigger:
+			if in_helm :
+				print("no longer helming ship")
+				in_helm = false
+				ship.player_in_helm = false
+				ext_cam.enabled = false
+				$Camera.enabled = true
+			elif !in_helm:
+				print("now helming ship")
+				in_helm = true
+				ship.player_in_helm = true
+				$Camera.enabled = false
+				ext_cam.enabled = true
+					
 
 func _physics_process(delta):
 	if !in_helm :
@@ -60,11 +78,17 @@ func _physics_process(delta):
 		var orientation_dir = Vector2.ZERO
 		var input_direction = Input.get_vector("left", "right", "up", "down")
 		if on_floor :
-				velocity = input_direction.normalized().rotated(ship.transform.get_rotation()) * SPEED
+			velocity = input_direction.normalized().rotated(ship.transform.get_rotation()) * move_speed
 		else:
-				velocity = input_direction.normalized() * SPEED
+			velocity = input_direction.normalized() * move_speed
 		if input_direction != Vector2.ZERO:
 			$BodySprite.rotation = lerp_angle(deg_to_rad($BodySprite.rotation_degrees), input_direction.angle(), .2)
+			if sprinting:
+				$AnimationPlayer.play("run")
+			else:
+				$AnimationPlayer.play("walk")
+		else:
+			$AnimationPlayer.stop()
 		move_and_slide()
 	
 
@@ -77,3 +101,4 @@ func _helm_exited(body):
 #	work around for area_entered emitting when ship moving fast
 	if !in_helm:
 		in_helm_trigger = false
+		
